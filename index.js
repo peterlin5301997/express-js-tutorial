c6a41988b1622d99498db2973575d62d689733e7
Flatiron Access Labs <flatironaccesslabs@Flatirons-MacBook-Air.local>
2019-12-21 12:19:18 -0500

    adds GET requests, Logger.js, Members.js, Middleware.js, npm installs nodemon & moment

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const path = require('path');
+const members = require('./Members'); // imports Members data
+const logger = require('./Logger'); // imports Logger
+
+const app = express();
+
+// Init Middleware
+app.use(logger)
+
+// Gets All Members
+app.get('/api/members', (req, res) => {
+  res.json(members);
+});
+
+// Get Single Member
+app.get('/api/members/:id', (req, res) => {
+
+  const found = members.some(member => {
+    member.id == req.params.id
+  })
+
+  if (found) {
+    res.json(members.filter(member => {
+      return member.id == req.params.id
+    }))
+  } else {
+    res.status(400).json({ msg: `No member with the id of ${req.params.id}` })
+  }
+});
+
+// Set Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
